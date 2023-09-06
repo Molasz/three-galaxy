792984d83b8ff9fdcbc978a717eb1044ab7da8ae
Molasz <martiolasz@gmail.com>
2023-09-06 02:48:22 +0200

    test

--- src/shaders/sun/vertex.glsl
+++ src/shaders/sun/vertex.glsl
@@ -7,7 +7,7 @@ void main() {
     float elevation = texture2D(uTexture, uv).r;
     vElevation = elevation;
 
-    vec3 pos = position + normal * elevation * 0.5  * sin(uTime * 0.001);    
+    vec3 pos = position + normal * elevation * (sin(uTime * 0.001) + 0.5) * 0.5;    
     gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
     
 }
